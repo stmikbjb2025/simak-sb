ffd425d809c8a0322232cacdd8b6f82dab482c81
stmikbjb2025 <stmikbjb2025@gmail.com>
2025-11-20 18:10:37 +0800

    edit ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,22 @@
 module.exports = {
-  apps: [
-    {
-      name: 'simak-web',
-      script: 'npm',
-      args: 'start',
-      // Next.js biasanya berjalan di port 3000
-      env: {
-        PORT: 3000,
-        NODE_ENV: 'production',
-      },
-    },
-    {
-      name: 'simak-scheduler',
-      script: 'npm',
-      args: 'run scheduler',
-      // Restart scheduler jika crash
-      autorestart: true,
-      watch: false,
-    },
-  ],
+	apps: [
+		{
+			name: 'simak-sb',
+			script: 'npm',
+			args: 'start',
+			// Next.js biasanya berjalan di port 3000
+			env: {
+				PORT: 3000,
+				NODE_ENV: 'production',
+			},
+		},
+		{
+			name: 'simak-scheduler-sb',
+			script: 'npm',
+			args: 'run scheduler',
+			// Restart scheduler jika crash
+			autorestart: true,
+			watch: false,
+		},
+	],
 };
